8a5abe594bb225b8311cd8deeff77ab92595b877
rodrigo.mora <rodrigo.mora@planetmedia.com.mx>
2019-10-01 18:42:04 -0500

    lislta e inputs dinamicos

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import './Transaction.dart';
-import 'package:intl/intl.dart';
+import './widgets/user_transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -20,14 +18,8 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final List<Transaction> transactions = [
-    Transaction(id: 't1', tile: 'Nike', amount: 69.99, date: DateTime.now()),
-    Transaction(id: 't2', tile: 'Adidas', amount: 70.50, date: DateTime.now()),
-    Transaction(id: 't3', tile: 'Fila', amount: 48.00, date: DateTime.now()),
-    Transaction(id: 't4', tile: 'Panam', amount: 35.00, date: DateTime.now()),
-    Transaction(id: 't5', tile: 'Convers', amount: 42.99, date: DateTime.now()),
-    Transaction(id: 't6', tile: 'Puma', amount: 65.00, date: DateTime.now()),
-  ];
+  // final List<Transaction> transactions = [
+  // ];
 
   // String titleImput; 
   // String amountInput; 
@@ -53,79 +45,7 @@ class MyHomePage extends StatelessWidget {
                   elevation: 5,
                 ),
               ),
-              Card(
-                elevation: 5,
-                child: Container(
-                  padding: EdgeInsets.all(10),
-                  
-                  child: Column(
-                   crossAxisAlignment: CrossAxisAlignment.end,
-                    children: <Widget>[
-                      TextField(
-                        decoration: InputDecoration(labelText: 'Title'),
-                        controller: titleController,
-                         // onChanged: (val){
-                          //titleImput = val;
-                        //},
-                      ),
-                      TextField(
-                        decoration: InputDecoration(labelText: 'Amount'),
-                        controller: amountController,
-                        // onChanged: (val)  
-                        // =>  amountInput = val
-                        // ,
-                      ),
-                      FlatButton(
-                        child: Text('Add Transaction'),
-                        textColor: Colors.purple,
-                        onPressed: () {
-                          print(titleController.text);
-                          print(amountController.text); 
-                          //print(titleImput);
-                          //print(amountInput);
-                        },
-                      )
-                    ],
-                  ),
-                ),
-              ),
-              Column(
-                children: transactions.map((tran) {
-                  return Card(
-                      child: Row(
-                    children: <Widget>[
-                      Container(
-                        margin:
-                            EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                        decoration: BoxDecoration(
-                            border: Border.all(color: Colors.purple, width: 2)),
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          '\$${tran.amount}',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                              color: Colors.purple),
-                        ),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            tran.tile,
-                            style: TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.bold),
-                          ),
-                          Text(
-                            DateFormat('dd/MM/yyyy').format(tran.date),
-                            style: TextStyle(color: Colors.grey),
-                          )
-                        ],
-                      )
-                    ],
-                  ));
-                }).toList(),
-              )
+              UserTransaction()
             ]));
   }
 }
